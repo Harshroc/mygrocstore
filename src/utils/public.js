5beb43fe8598e3d4a8c790d18c40b0314e0e3511
Harshroc <93042806+Harshroc@users.noreply.github.com>
2022-02-26 18:30:58 +0530

    Update public.js

--- src/utils/public.js
+++ src/utils/public.js
@@ -1,1 +1,2 @@
-export const API_URL = "https://vast-reaches-68978.herokuapp.com";
+// export const API_URL = "https://vast-reaches-68978.herokuapp.com";
+export const API_URL = "https://mygrocstore.herokuapp.com";
